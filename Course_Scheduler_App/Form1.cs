4e92f961dcb862967df2dc7b5f3ae32041b57e6d
Ross Jacobson <74107876+Ross-J@users.noreply.github.com>
2021-11-28 23:16:39 -0600

    Add project files.

---
+++ Course_Scheduler_App/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Course_Scheduler_App
+{
+    public partial class Form1 : Form
+    {
+        public static string fileContent = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file 
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream 
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                        //displays contents of the file
+
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var m = new Form2();
+            m.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Course_Scheduler_App/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Course_Scheduler_App
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            richTextBox1.Text = Form1.fileContent;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var m = new Form1();
+            m.Show();
+            this.Hide();
+        }
+    }
+}
