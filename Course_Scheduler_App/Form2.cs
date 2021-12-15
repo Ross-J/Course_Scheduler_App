4e4de08f9b38519fcf9b164b20102c4fd797bade
Calvin Grzan <calvin.grzan@cuw.edu>
2021-12-14 21:25:50 -0600

    Form 2 Bug

--- Course_Scheduler_App/Form2.cs
+++ Course_Scheduler_App/Form2.cs
@@ -38,42 +38,49 @@ namespace Course_Scheduler_App
                         var label = new Label();
                         label.Text = kv.Subject;
                         this.Controls.Add(label);
-                        label.Location = new Point(20, 20);
+                        label.Location = new System.Drawing.Point(200 * (i + 1), 15 * (i + 1));
                         label.Size = new System.Drawing.Size(41, 13);
 
                         var label2 = new Label();
                         label2.Text = kv.Number;
                         this.Controls.Add(label2);
+                        label2.Location = new System.Drawing.Point(300 * (i + 1), 15 * (i + 1));
                         label2.Size = new System.Drawing.Size(41, 13);
 
                         var label3 = new Label();
                         label3.Text = kv.Instructor;
                         this.Controls.Add(label3);
+                        label3.Location = new System.Drawing.Point(400 * (i + 1), 15 * (i + 1));
                         label3.Size = new System.Drawing.Size(41, 13);
 
                         var label4 = new Label();
                         label4.Text = kv.Days;
                         this.Controls.Add(label4);
+                        label4.Location = new System.Drawing.Point(500 * (i + 1), 15 * (i + 1));
                         label4.Size = new System.Drawing.Size(41, 13);
 
                         var label5 = new Label();
                         label5.Text = kv.StartTime;
                         this.Controls.Add(label5);
+                        label5.Location = new System.Drawing.Point(600 * (i + 1), 15 * (i + 1));
                         label5.Size = new System.Drawing.Size(41, 13);
 
                         var label6 = new Label();
                         label6.Text = kv.EndTime;
                         this.Controls.Add(label6);
+                        label6.Location = new System.Drawing.Point(700 * (i + 1), 15 * (i + 1));
                         label6.Size = new System.Drawing.Size(41, 13);
 
                         var label7 = new Label();
                         label7.Text = kv.Building;
                         this.Controls.Add(label7);
+                        label7.Location = new System.Drawing.Point(800 * (i + 1), 15 * (i + 1));
                         label7.Size = new System.Drawing.Size(41, 13);
 
                         var label8 = new Label();
                         label8.Text = kv.Room;
                         this.Controls.Add(label8);
+                        label8.Location = new System.Drawing.Point(900 * (i + 1), 15 * (i + 1));
                         label8.Size = new System.Drawing.Size(41, 13);
                         
 
